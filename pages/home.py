cd6c0e70514d96e200414028ef6a363524d94d17
anitacwibowo <anitacwibowo@gmail.com>
2025-07-19 09:59:05 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -17,7 +17,9 @@ st.markdown("""
 st.markdown("""
 <h3 style='font-family:Arial; color:#333;'>SELAMAT DATANG!</h3>
 
-<span style='font-size:15px;'>Ini adalah Sistem Analisis Sentimen Pengunjung Hotel Bahasa Indonesia yang dikembangkan sebagai bagian dari penelitian tugas akhir yang bertujuan untuk mengidentifikasi dan mengklasifikasikan opini pengunjung hotel berdasarkan ulasan teks dari berbagai platform digital.</span></p>            
+<span style='font-size:15px;'>Ini adalah</span></p> 
+<p><strong style='font-size:16px;'>Sistem Analisis Sentimen Pengunjung Hotel Bahasa Indonesia</strong><br> 
+<span style='font-size:15px;'>yang dikembangkan sebagai bagian dari penelitian tugas akhir yang bertujuan untuk mengidentifikasi dan mengklasifikasikan opini pengunjung hotel berdasarkan ulasan teks dari berbagai platform digital.</span></p>            
 
 <span style='font-size:15px;'>Sistem ini menggunakan algoritma Long Short-Term Memory (LSTM), sebuah metode deep learning yang efektif dalam memproses data urutan (sequential data) seperti teks. 
             Model dilatih menggunakan 8.829 data ulasan berbahasa Indonesia yang telah melalui tahap pre-processing.
